b1841b7f2998a3ef90739b6d2a3f62033f5fb184
Andrew TOwnsley <atownsley86@gmail.com>
2021-05-05 22:28:53 -0600

    array methods

---
+++ app.js
@@ -0,0 +1,19 @@
+// // You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// // Your task is to return an object (associative array in PHP) which includes the count of each coding language represented at the meetup.
+
+// // For example, given the following input array:
+
+
+// const list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
+//   { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
+//   { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
+//   { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
+// ];
+
+// function countLanguages(list) {
+//     let count = {};
+//     list.forEach(item => count[item.language] = (count[item.language] || 0) + 1);
+//     return count;
+// }
